0b26166c6da74dcb978a5fee3bb1b6d312bd85d3
rizkyanaazka <rizkyanaazka@gmail.com>
2025-07-14 11:29:53 +0700

    init

---
+++ config/cors.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://localhost:5173', 'https://web-bps-frontend-omega.vercel.app'],
+    
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
