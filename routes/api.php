1f548712e3c1d9b4c892563bb40f1f3b6b8a6229
rizkyanaazka <rizkyanaazka@gmail.com>
2025-07-14 13:47:41 +0700

    all commit

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,15 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/publikasi/{id}', [PublikasiController::class, 'update']); 
     Route::delete('/publikasi/{id}', [PublikasiController::class, 'destroy']); 
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
